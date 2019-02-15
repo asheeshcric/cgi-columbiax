905da83824a66c26629c15cf596ef2731b4d9885
Ashish <ashiz2013@gmail.com>
2019-02-15 14:42:11 +0545

    Refactor ExplicitEuler class

--- t1m1/FOSSSim/ExplicitEuler.cpp
+++ t1m1/FOSSSim/ExplicitEuler.cpp
@@ -2,10 +2,20 @@
 #include <iostream>
 using namespace std;
 
+// Write code to save the kinetic energy (in the stepper, i.e this file), get the kinetic energy from TwoDScene::computeKineticEnergy(),
+// and accumulate dt to track the simulation time
+            /* Format
+            # Time     KineticEnergy
+            time0      energy0
+            time1      energy1
+            ...        ...
+
+            */
+
 bool ExplicitEuler::stepScene( TwoDScene& scene, scalar dt )
 {
     // Your code goes here!
-    
+
     // Some tips on getting data from TwoDScene:
     // A vector containing all of the system's position DoFs. x0, y0, x1, y1, ...
     //VectorXs& x = scene.getX();
@@ -15,7 +25,11 @@ bool ExplicitEuler::stepScene( TwoDScene& scene, scalar dt )
     //const VectorXs& m = scene.getM();
     // Determine if the ith particle is fixed
     // if( scene.isFixed(i) )
-    
+
+
+    // This class ExplicitEuler is meant to update the postion and velocity at each timestep using Explicit Euler method
+    // Remember to account for the Forces that are acting on the particles for the change in velocity
+
     VectorXs& x = scene.getX();
     VectorXs& v = scene.getV();
     const VectorXs& m = scene.getM();
@@ -30,19 +44,21 @@ bool ExplicitEuler::stepScene( TwoDScene& scene, scalar dt )
     // Force is negative the energy gradient
     F *= -1.0;
 
-    // Zero the force for fixed DoFs
-    for( int i = 0; i < scene.getNumParticles(); ++i ) if( scene.isFixed(i) ) F.segment<2>(2*i).setZero();
+    // Set the force for fixed DoF to zero
+    for( int i = 0; i < scene.getNumParticles(); i++ ) {
+        if( scene.isFixed(i) ) {
+            F.segment<2>(2*i).setZero();
+        }
+    }
 
     // New position for the next time-step based on the current velocity
     x += dt*v;
 
     // Normalize the Force vector by dividing with the mass array (done to extract the acceleration part from the Force)
     F.array() /= m.array();
-    
+
     // New velocity for the next time-step based on the acceleration
     v += dt*F;
-
-    cout << "x = " << x << endl << ", v = " << v << endl << ", m = " << m << endl;
 
     return true;
 }
