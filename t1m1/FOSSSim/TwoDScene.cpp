2a31df0da1ee1647cafd39ed083beebe67979733
Ashish <ashiz2013@gmail.com>
2019-02-15 14:41:51 +0545

    Add code to compute the kinetic energy of the particles

--- t1m1/FOSSSim/TwoDScene.cpp
+++ t1m1/FOSSSim/TwoDScene.cpp
@@ -1,6 +1,12 @@
 #include "TwoDScene.h"
 
+#include <iostream>
+using namespace std;
+
 scalar TwoDScene::computeKineticEnergy() const
 {
-    return -1.0;
+    // This class is used to compute the Kinetic Energy of the system T=1/2 mv.v
+    // In absence of external forces, the kinetic energy remains constant -- Newton's 1st law
+    // For sanity check, execute the scene InertiaTests/test01explicit.xml and print the kinetic energy at each timestep (to verify that it is constant)
+    return 0.5 * m_v.dot(VectorXs(m_m.array() * m_v.array()));
 }
